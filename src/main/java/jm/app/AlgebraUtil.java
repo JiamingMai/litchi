81c0fdf22e61fb97ee4a479822e4182907dcff25
麦嘉铭 <jiamingmai@maijiamingdeMacBook.local>
2019-02-15 22:33:53 +0800

    added some methods in AlgebraUtil.java

--- src/main/java/jm/app/AlgebraUtil.java
+++ src/main/java/jm/app/AlgebraUtil.java
@@ -1,5 +1,7 @@
 package jm.app;
 
+import java.math.BigDecimal;
+
 public class AlgebraUtil {
 
     public final static Matrix identityMatrix(int dimension) {
@@ -8,6 +10,24 @@ public class AlgebraUtil {
             identityMatrix.setValue(i, i, 1.0);
         }
         return identityMatrix;
+    }
+
+    public final static Matrix add(Matrix a, Matrix b) {
+        if (a == null || b == null) {
+            return null;
+        }
+        if (a.getRowNum() != b.getRowNum() || a.getColNum() != b.getColNum()) {
+            // TODO: throw exception here
+            return null;
+        }
+        Matrix resultMat = new Matrix(a.getRowNum(), a.getColNum());
+        for (int i = 0; i < resultMat.getRowNum(); i++) {
+            for (int j = 0; j < resultMat.getColNum(); j++) {
+                BigDecimal value = a.getValue(i, j).add(b.getValue(i, j));
+                resultMat.setValue(i, j, value);
+            }
+        }
+        return resultMat;
     }
 
 
