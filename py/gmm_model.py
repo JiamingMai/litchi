224b3fad1de507f8bc2cc08833aa83f821b8e6dc
麦嘉铭 <jiamingmai@163.com>
2019-04-27 21:35:39 +0800

    updated gmm_model_test.py and gmm_model.py

--- py/gmm_model.py
+++ py/gmm_model.py
@@ -14,7 +14,7 @@ def cluster(x, k, epoch_num):
         sigma.append(sigma_i)
 
     for e in range(epoch_num):
-        print("epoch #%d" %(e+1))
+        print("epoch #%d" % (e + 1))
         new_u = []
         new_sigma = []
         for i in range(k):
@@ -29,7 +29,7 @@ def cluster(x, k, epoch_num):
                 new_ui = new_ui + pji * x[j]
                 ni = ni + pji
                 new_sigma_i = new_sigma_i + pji * np.array(np.transpose(np.mat(x[j] - u[i])).dot(np.mat(x[j] - u[i])))
-            print("cluster #%d have %d samples" %(i, ni))
+            print("cluster #%d have %d samples" % (i, ni))
             new_ui = new_ui / ni
             new_sigma_i = new_sigma_i / ni
             new_u.append(new_ui)
@@ -41,9 +41,25 @@ def cluster(x, k, epoch_num):
     return u, sigma
 
 
+def cluster_with_u_and_sigma(x, k, u, sigma):
+    x_classes = {}
+    for s in range(k):
+        x_classes[s] = []
+    for j in range(x.shape[0]):
+        max_probability = -1
+        best_class = -1
+        for s in range(k):
+            pjs = gaussian_fun(x[j], u[s], sigma[s])
+            if (pjs > max_probability):
+                max_probability = pjs
+                best_class = s
+        x_classes[best_class].append(j)
+    return x_classes
+
+
 def gaussian_fun(x, u, sigma):
     d = len(x)
-    left_term = 1 / (np.power(2*np.pi, d/2) * np.power(np.linalg.det(sigma), 0.5))
+    left_term = 1 / (np.power(2 * np.pi, d / 2) * np.power(np.linalg.det(sigma), 0.5))
     right_term = np.exp(-0.5 * np.dot(np.dot(np.transpose(x - u), np.linalg.inv(sigma)), x - u))
     p = left_term * right_term
     return p
--- py/gmm_model_test.py
+++ py/gmm_model_test.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import py.gmm_model
 
+
 def test_gmm_model():
     file_path = os.getcwd() + "/../src/main/resources/cluster_test_data.csv"
     x = np.loadtxt(file_path, delimiter=",",usecols=(1, 2), unpack=True)
@@ -9,6 +10,8 @@ def test_gmm_model():
     k = 3
     epoch_num = 50
     u, sigma = py.gmm_model.cluster(x, k, epoch_num)
+    x_classes = py.gmm_model.cluster_with_u_and_sigma(x, k, u, sigma)
+    print(x_classes)
 
 
 if __name__ == "__main__":
