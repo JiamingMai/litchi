5bbcd04c4ac9e83b34ada171c68eea84cf8a927f
麦嘉铭 <jiamingmai@163.com>
2019-05-13 13:46:03 +0800

    updated genetic_algorithm_optimizer.py

--- py/genetic_algorithm_optimizer.py
+++ py/genetic_algorithm_optimizer.py
@@ -24,9 +24,41 @@ class GeneticAlgorithmOptimizer(opt.Optimizer):
             denormalized_params[0, i] = new_value
         return denormalized_params
 
-    def select_operator(self, rmse_function, params_population, args):
+    def select_operator(self, loss_function, params_population, args):
+        params_num = params_population.shape[1]
+        ratios = np.random.rand(self.seed_num)
+        fitness_values = np.random.rand(self.seed_num)
+        sum = 0.0
+        for i in range(self.seed_num):
+            denormalized_params = self.denormalize_params(params_population[i, :])
+            fitness_values[i] = 1.0 / loss_function.fun(denormalized_params, args)
+            sum = sum + fitness_values[i]
+        for i in range(self.seed_num):
+            ratios[i] = fitness_values[i] / sum
 
-        return
+        # calculate the scopes
+        last_threshold = 0.0
+        for i in range(self.seed_num):
+            ratios[i] = ratios[i] + last_threshold
+            last_threshold = ratios[i]
+
+        # do selection
+        selected_indices = []
+        for i in range(self.seed_num):
+            random_value = np.random.rand()
+            for j in range(self.seed_num):
+                if random_value < ratios[j]:
+                    selected_indices.append(j)
+                    break
+        selected_indices = np.array(selected_indices)
+
+        for i in range(self.seed_num):
+            params = np.random.rand(1, params_num)
+            for j in range(params_num):
+                params[0, j] = params_population[selected_indices[i], j]
+            params_population[i, :] = params
+
+        return params_population
 
     def crossover_operator(self, params_population):
         params_num = params_population.shape[1]
@@ -56,9 +88,18 @@ class GeneticAlgorithmOptimizer(opt.Optimizer):
     def cross(self, item1, item2, bit_num):
         cross_bits = np.random.randint(bit_num)
         new_items = np.random.rand(2, bit_num)
+
         # generate the first one
+        for i in range(cross_bits):
+            new_items[0, i] = item1[0, i]
+        for i in range(cross_bits + 1, bit_num):
+            new_items[0, i] = item2[0, i]
 
         # generate the second one
+        for i in range(cross_bits):
+            new_items[1, i] = item2[0, i]
+        for i in range(cross_bits + 1, bit_num):
+            new_items[1, i] = item1[0, i]
 
         return new_items
 
